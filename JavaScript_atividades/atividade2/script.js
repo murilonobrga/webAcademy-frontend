d84df58e6175d74b2f249fee41d59675ddc524fd
murilonobrga <murilonobrega@icloud.com>
2026-05-28 14:32:22 -0500

    feat: adicionando coisas de JS

---
+++ JavaScript_atividades/atividade2/script.js
@@ -0,0 +1,24 @@
+console.log("Boa noite, SEXTOU");
+
+//console 
+
+console.error("BURRO!")
+console.warn("ta fazendo coisa errada viu")
+
+//variáveis
+
+var l = 5;
+let x = 10;
+let frutas = ["maça", "banana", "pera", "goiaba"]
+const b = 20;
+
+console.log(x);
+console.log(l);
+console.log(b);
+
+//tipos de dados: texto
+
+const name = "Murilo";
+console.log(name);
+
+//tipos de dados: 
